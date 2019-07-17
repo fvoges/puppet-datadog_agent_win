a476c05e1048fe383bca436cd4ba5237a43b7964
Federico Voges <federico@puppet.com>
2019-07-17 22:29:36 +0200

    fix rubocop failure

--- spec/classes/datadog_agent_win_spec.rb
+++ spec/classes/datadog_agent_win_spec.rb
@@ -4,7 +4,7 @@ describe 'datadog_agent_win' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) { { 'api_key' => 'TESTME'} }
+      let(:params) { { 'api_key' => 'TESTME' } }
 
       it { is_expected.to compile }
     end
